2c07e96d227442d50602d60d6658ccd4a146e90b
Karen Sofía González <6591085+karencilla-unlam@users.noreply.github.com>
2022-11-27 22:14:54 -0300

    Delete ClienteNoAtendidoException.java

--- src/ar/edu/unlam/pb2/serviciomecanico/ClienteNoAtendidoException.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlam.pb2.serviciomecanico;
-
-public class ClienteNoAtendidoException extends Exception {
-	public ClienteNoAtendidoException(String mensaje) {
-		super(mensaje);
-	}
-}
