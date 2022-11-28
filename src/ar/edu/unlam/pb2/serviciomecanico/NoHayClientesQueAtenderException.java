6aee2dbff86d8f38e47076210a54eb385052cc65
Karen Sofía González <6591085+karencilla-unlam@users.noreply.github.com>
2022-11-27 22:15:29 -0300

    Create NoHayClientesQueAtenderException.java

---
+++ src/ar/edu/unlam/pb2/serviciomecanico/NoHayClientesQueAtenderException.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.pb2.serviciomecanico;
+
+public class NoHayClientesQueAtenderException extends Exception {
+	public NoHayClientesQueAtenderException(String mensaje) {
+		super(mensaje);
+	}
+
+}
