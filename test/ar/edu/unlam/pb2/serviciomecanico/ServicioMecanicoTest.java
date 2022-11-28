28c99cb3764657adec639348f15c9f14a3cc0da2
Karen Sofía González <6591085+karencilla-unlam@users.noreply.github.com>
2022-11-27 22:11:31 -0300

    Update with fixes in tests (using exceptions)

--- test/ar/edu/unlam/pb2/serviciomecanico/ServicioMecanicoTest.java
+++ test/ar/edu/unlam/pb2/serviciomecanico/ServicioMecanicoTest.java
@@ -5,64 +5,75 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServicioMecanicoTest {
 	
 	@Test
 	public void queSePuedaAgregarUnCliente() {
-		Cliente miCliente = new cliente("Karina",123456789);
+		Cliente miCliente = new Cliente("Karina",123456789);
 		Set<Cliente> listaClientes = new HashSet<Cliente>();
 		ServicioMecanico servicio = new ServicioMecanico(listaClientes,0);
 		servicio.nuevoCliente(miCliente);
 		assertTrue(listaClientes.equals(servicio.getClientes()));
 	}
 	
-	@Test
-	public void queSePuedaAtenderAunCliente() {
-		Cliente otroCliente = new cliente("Lorena",123456555);
+	@Test (expected=NoHayClientesQueAtenderException.class)
+	public void queSePuedaAtenderAunCliente() throws NoHayClientesQueAtenderException {
+		Cliente otroCliente = new Cliente("Lorena",123456555);
 		Set<Cliente> listaClientes = new HashSet<Cliente>();
 		ServicioMecanico otroServicio = new ServicioMecanico(listaClientes,0);
-		servicio.nuevoCliente(otroCliente);
-		assertTrue(listaClientes.equals(otroServicio.atenderCliente(otroCliente)));
+		// servicio.nuevoCliente(otroCliente);
+		assertTrue(listaClientes.equals(otroServicio.atenderCliente()));
 	}
 	
 	@Test
-	public void queSePuedaObtenerCantidadDeClientesAtendidos() {
+	public void queSePuedaObtenerCantidadDeClientesAtendidos() throws NoHayClientesQueAtenderException {
 		Integer valorEsperado=2;
-		Cliente miCliente = new cliente("Karina",123456789);
-		Cliente otroCliente = new cliente("Lorena",123456555);
+		Cliente miCliente = new Cliente("Karina",123456789);
+		Cliente otroCliente = new Cliente("Lorena",123456555);
 		Set<Cliente> listaClientes = new HashSet<Cliente>();
-		ServicioMecanico nuevoServicio = new ServicioMecanico(listaClientes,0);
+		ServicioMecanico servicio = new ServicioMecanico(listaClientes,0);
 		servicio.nuevoCliente(miCliente);
 		servicio.nuevoCliente(otroCliente);
-		servicio.atenderCliente();
-		servicio.atenderCliente();
-		assertTrue(nuevoServicio.obtenerCantidadClientesAtendidos()).equals(valorEsperado));
+		try {
+			servicio.atenderCliente();
+			servicio.atenderCliente();
+		}
+		catch (NoHayClientesQueAtenderException e) {
+			e.printStackTrace();
+		}
+		assertEquals(valorEsperado,servicio.obtenerCantidadClientesAtendidos());
 	}
-	
+
 	@Test
-	public void queSePuedaObtenertiempoDeEsperaDeAtendidos() {
-		Cliente miCliente = new cliente("Karina",123456789);
-		Cliente otroCliente = new cliente("Lorena",123456555);
+	public void queSePuedaObtenertiempoDeEsperaDeAtendidos() throws NoHayClientesQueAtenderException {
+		Cliente miCliente = new Cliente("Karina",123456789);
+		Cliente otroCliente = new Cliente("Lorena",123456555);
 		Set<Cliente> listaClientes = new HashSet<Cliente>();
 		ServicioMecanico nuevoServicio = new ServicioMecanico(listaClientes,0);
-		servicio.nuevoCliente(miCliente);
-		servicio.nuevoCliente(otroCliente);
-		servicio.atenderCliente();
-		servicio.atenderCliente();
-		assertTrue((nuevoServicio.tiempoDeEsperaAtendidos()).equals(0));
+		nuevoServicio.nuevoCliente(miCliente);
+		nuevoServicio.nuevoCliente(otroCliente);
+		try {
+			nuevoServicio.atenderCliente();
+			nuevoServicio.atenderCliente();
+		}
+		catch (NoHayClientesQueAtenderException e) {
+			e.printStackTrace();
+		}
+		assertNotNull(nuevoServicio.tiempoDeEsperaAtendidos());
 	}
 	
 	@Test
 	public void queSePuedaObtenertiempoDeEsperaDeNoAtendidos() {
-		Cliente miCliente = new cliente("Karina",123456789);
-		Cliente otroCliente = new cliente("Lorena",123456555);
+		Cliente miCliente = new Cliente("Karina",123456789);
+		Cliente otroCliente = new Cliente("Lorena",123456555);
 		Set<Cliente> listaClientes = new HashSet<Cliente>();
 		ServicioMecanico nuevoServicio = new ServicioMecanico(listaClientes,0);
-		servicio.nuevoCliente(miCliente);
-		servicio.nuevoCliente(otroCliente);
-		assertTrue((nuevoServicio.tiempoDeEsperaNoAtendidos()).equals(0));
+		nuevoServicio.nuevoCliente(miCliente);
+		nuevoServicio.nuevoCliente(otroCliente);
+		assertNotNull(nuevoServicio.tiempoDeEsperaNoAtendidos());
 	}
 
 }
