cfa18b51eab3ea9f0b47d65079ac75783269cc43
Karen Sofía González <6591085+karencilla-unlam@users.noreply.github.com>
2022-11-27 22:14:22 -0300

    Override methods and add wrapper classes

--- src/ar/edu/unlam/pb2/serviciomecanico/Cliente.java
+++ src/ar/edu/unlam/pb2/serviciomecanico/Cliente.java
@@ -1,10 +1,12 @@
 package ar.edu.unlam.pb2.serviciomecanico;
 
+import java.util.Objects;
+
 public class Cliente {
 	private String nombre;
 	private Integer nromovil;
-	private Reloj fhllegada;
-	private Reloj fhatencion;
+	private Long fhllegada;
+	private Long fhatencion;
 	private Boolean fueatendido;
 	
 	// Constructores
@@ -12,8 +14,8 @@ public class Cliente {
 	public Cliente(String nombre, Integer nromovil) {
 		this.nombre = nombre;
 		this.nromovil = nromovil;
-		this.fhllegada = 0;
-		this.fhatencion = 0;
+		this.fhllegada = 0L;
+		this.fhatencion = 0L;
 		this.fueatendido = false;
 	}
 
@@ -40,22 +42,22 @@ public class Cliente {
 	}
 
 
-	public Reloj getFhllegada() {
+	public long getFhllegada() {
 		return fhllegada;
 	}
 
 
-	public void setFhllegada(Reloj fhllegada) {
+	public void setFhllegada(Long fhllegada) {
 		this.fhllegada = fhllegada;
 	}
 
 
-	public Reloj getFhatencion() {
+	public long getFhatencion() {
 		return fhatencion;
 	}
 
 
-	public void setFhatencion(Reloj fhatencion) {
+	public void setFhatencion(Long fhatencion) {
 		this.fhatencion = fhatencion;
 	}
 
@@ -68,6 +70,24 @@ public class Cliente {
 	public void setFueatendido(Boolean fueatendido) {
 		this.fueatendido = fueatendido;
 	}
-	
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nombre, nromovil);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(nombre, other.nombre) && Objects.equals(nromovil, other.nromovil);
+	}
 
 }
